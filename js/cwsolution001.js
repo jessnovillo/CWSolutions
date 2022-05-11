a8833c42e0648e2e1dd431bb039619905a134dfd
jessnovillo <jnovillo66@gmail.com>
2022-05-10 23:04:23 -0700

    cw 7kyu push

--- js/cwsolution001.js
+++ js/cwsolution001.js
@@ -1,1 +1,9 @@
-//5/9/22    
+//5/10/22 -- Exes and Ohs 
+
+function XO(str){
+  let numX = str.toUpperCase().split('').filter(e => e ==='X').length
+  let numO = str.toUpperCase().split('').filter(f => f ==='O').length
+  if (numX === numO){
+        return true
+    } else { return false }
+}
