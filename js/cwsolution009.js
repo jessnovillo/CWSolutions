fa7c60de51a907e1cd13248e945c7ac4b5f1cdb7
jessnovillo <jnovillo66@gmail.com>
2022-05-18 23:02:56 -0700

    cw 8

---
+++ js/cwsolution009.js
@@ -0,0 +1,5 @@
+//cw Friend or Foe? 7kyu
+
+function friend(friends){
+  return friends.filter(e => e.length === 4 ? true : false )
+}
---
+++ js/cwsolution010.js
@@ -0,0 +1,5 @@
+// cw Double Char 8kyu
+
+function doubleChar(str) {
+ return str.split('').map(e => e+e).join('')
+}
